349eec2eb9ecd85817b2e2ae1aef8f9430bcd192
Thomas Calle <thos.calle@gmail.com>
2023-03-29 19:13:15 -0400

    comments added to describe functionality and purpose

--- highscores.js
+++ highscores.js
@@ -1,9 +1,20 @@
+// Retrieve high scores from local storage and parse them; if none exist, use an empty array
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-highScores.sort((a,b)=>a-b)
 
+// Sort the high scores in ascending order
+highScores.sort((a, b) => a - b);
+
+// Get the leaderboard_tracker element from the HTML
 const leaderBoards = document.getElementById("leaderboard_tracker");
+
+// Loop through each high score in the sorted array
 highScores.forEach(score => {
+    // Create a new list item element for the current score
     const listItem = document.createElement("li");
+
+    // Set the content of the list item to display the name and score
     listItem.textContent = `name: ${score.initials} score: ${score.score}`;
+
+    // Append the list item to the leaderboard_tracker element
     leaderBoards.appendChild(listItem);
 });
--- sandb.js
+++ sandb.js
@@ -1,61 +1,50 @@
-
+// Define quiz questions, choices, and answers
 const questions = [
-    {
-      question: 'What is the primary purpose of a Web API?',
-      choices: [
-        'A. To design web pages',
-        'B. To facilitate communication between client and server',
-        'C. To optimize search engine results',
-        'D. To secure websites from hackers',
-      ],
-      answer: 'B',
-    },
-    {
-      question: 'Which protocol is commonly used for Web APIs?',
-      choices: [
-        'A. FTP',
-        'B. SMTP',
-        'C. HTTP',
-        'D. IMAP',
-      ],
-      answer: 'C',
-    },
-    {
-      question: 'What does REST stand for in the context of Web APIs?',
-      choices: [
-        'A. Reliable Event Sourcing Technology',
-        'B. Request-Event-Source-Transfer',
-        'C. Representational State Transfer',
-        'D. Recursive Standard Transmission',
-      ],
-      answer: 'C',
-    },
-    {
-      question: 'Which of the following HTTP methods is typically used to update a resource in a RESTful API?',
-      choices: [
-        'A. GET',
-        'B. POST',
-        'C. PUT',
-        'D. DELETE',
-      ],
-      answer: 'C',
-    },
-    {
-      question: 'What data format is most commonly used for exchanging data in Web APIs?',
-      choices: [
-        'A. CSV',
-        'B. JSON',
-        'C. XML',
-        'D. HTML',
-      ],
-      answer: 'B',
-    },
-  ];  
-
+  {
+    question: "What is the primary purpose of a Web API?",
+    choices: [
+      "A. To design web pages",
+      "B. To facilitate communication between client and server",
+      "C. To optimize search engine results",
+      "D. To secure websites from hackers",
+    ],
+    answer: "B",
+  },
+  {
+    question: "Which protocol is commonly used for Web APIs?",
+    choices: ["A. FTP", "B. SMTP", "C. HTTP", "D. IMAP"],
+    answer: "C",
+  },
+  {
+    question: "What does REST stand for in the context of Web APIs?",
+    choices: [
+      "A. Reliable Event Sourcing Technology",
+      "B. Request-Event-Source-Transfer",
+      "C. Representational State Transfer",
+      "D. Recursive Standard Transmission",
+    ],
+    answer: "C",
+  },
+  {
+    question:
+      "Which of the following HTTP methods is typically used to update a resource in a RESTful API?",
+    choices: ["A. GET", "B. POST", "C. PUT", "D. DELETE"],
+    answer: "C",
+  },
+  {
+    question:
+      "What data format is most commonly used for exchanging data in Web APIs?",
+    choices: ["A. CSV", "B. JSON", "C. XML", "D. HTML"],
+    answer: "B",
+  },
+];
+// This code initializes the variables for the timer, the current question index, and the score.
 let timer = 60;
 let currentQuestionIndex = 0;
 let score = 0;
 let timerInterval;
+// This function starts the quiz by hiding the intro container and displaying the question container.
+// It also resets the timer and displays the first question.
 const startQuiz = () => {
   document.querySelector(".intro-container").hidden = true;
   document.querySelector(".question-container").hidden = false;
@@ -63,13 +52,14 @@ const startQuiz = () => {
   startTimer();
   displayQuestion();
 };
-
+// This function displays the current question and its answer choices.
+// It also adds a click event listener to each answer choice button.
 const displayQuestion = () => {
   const currentQuestion = questions[currentQuestionIndex];
   document.getElementById("question").innerText = currentQuestion.question;
   const choicesDiv = document.getElementById("choices");
   choicesDiv.innerHTML = "";
-
+  // loop through each answer choice in the current question object and create a button element for each one
   currentQuestion.choices.forEach((choice, index) => {
     const button = document.createElement("button");
     button.innerText = choice;
@@ -78,32 +68,35 @@ const displayQuestion = () => {
   });
 };
 
+// This function handles the user's answer to the current question.
+// It updates the score and timer variables based on the answer, and displays the next question or ends the quiz if there are no more questions.
 const handleAnswer = (index) => {
   if (index === questions[currentQuestionIndex].answer) {
     score++;
   } else {
     timer -= 10;
   }
-
+  // move on to the next question
   currentQuestionIndex++;
 
+  // This function ends the quiz by stopping the timer, hiding the question container, and displaying the final score container.
   if (currentQuestionIndex < questions.length) {
     displayQuestion();
   } else {
     endQuiz();
   }
 };
-
+// This function ends the quiz by stopping the timer, hiding the question container, and displaying the final score container.
 const endQuiz = () => {
   clearInterval(timerInterval);
   document.querySelector(".question-container").hidden = true;
   document.querySelector(".final-score-container").hidden = false;
   document.getElementById("final-score").innerText = score;
 };
-
+// This function saves the user's score and initials to local storage.
 const saveScore = () => {
   const initials = document.getElementById("initials").value;
-  if (initials === '') return; // Prevent saving empty initials
+  if (initials === "") return; // Prevent saving empty initials
   const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
   const newScore = { initials, score };
 
@@ -113,22 +106,24 @@ const saveScore = () => {
   localStorage.setItem("highScores", JSON.stringify(highScores));
   window.location.assign("/highscores.html");
 };
-
+// Function to start the timer countdown
 const startTimer = () => {
   const timerDiv = document.getElementById("timer");
 
   const countdown = () => {
-    timer--;
+    timer--; // decrement the timer by 1 second
     timerDiv.innerText = `Time left: ${timer}`;
-
+    // if the timer reaches 0 or less
     if (timer <= 0) {
+      // stop the timer countdown
       clearInterval(timerInterval);
+      // end the quiz
       endQuiz();
     }
   };
 
-   timerInterval = setInterval(countdown, 1000);
+  timerInterval = setInterval(countdown, 1000);
 };
-
+// Event listeners for starting the quiz and submitting the score
 document.getElementById("start-quiz").addEventListener("click", startQuiz);
 document.getElementById("submit-score").addEventListener("click", saveScore);
